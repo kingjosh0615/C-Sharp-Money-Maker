e8b49607e99efc49434a538984213a26c7176fd6
Wiles_Joshua <Wiles_Joshua@edu.mcctc.net>
2020-09-03 10:33:44 -0400

    Final

--- CSharpMoneyMaker/CSharpMoneyMaker/Program.cs
+++ CSharpMoneyMaker/CSharpMoneyMaker/Program.cs
@@ -2,11 +2,33 @@
 
 namespace CSharpMoneyMaker
 {
-    class Program
+    class MainClass
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            
+            Console.WriteLine("Welcome to Money Maker!");
+
+            Console.WriteLine("Type in the amount of money you'd like to convert.");
+            double amountToConvert = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"{amountToConvert} is equal to...");
+
+            // coin worths
+            double gold = 10;
+            double silver = 5;
+
+            // math :)
+            double goldCoins = Math.Floor(amountToConvert / gold);
+            double coinRemaining = amountToConvert % gold;
+            double silverCoins = Math.Floor(coinRemaining / silver);
+            coinRemaining = coinRemaining % silver;
+
+            // print result
+            Console.WriteLine($"Gold Coins: {goldCoins}");
+            Console.WriteLine($"Silver Coins: {silverCoins}");
+            Console.WriteLine($"Bronze Coins: {coinRemaining}");
+
+
+
         }
     }
 }
